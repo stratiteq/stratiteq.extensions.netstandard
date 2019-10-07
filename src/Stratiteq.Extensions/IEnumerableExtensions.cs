3667c2099067377e70213f5487612e8e5346467f
Peter Nylander <pnylander@stratiteq.com>
2019-10-07 12:54:13 +0200

    Added solution and project

---
+++ src/Stratiteq.Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Stratiteq Sweden AB. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Stratiteq.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Apply an action to each element.
+        /// </summary>
+        /// <typeparam name="T">Type of elements.</typeparam>
+        /// <param name="enumerable">Collection to iterate.</param>
+        /// <param name="action">Action to apply on each element.</param>
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (T item in enumerable)
+            {
+                action(item);
+            }
+        }
+    }
+}
