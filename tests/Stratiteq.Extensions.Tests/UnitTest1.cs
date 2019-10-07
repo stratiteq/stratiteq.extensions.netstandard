6be01b55ca0a3776c5cbeb0c3ebbcafc472a3ee7
Peter Nylander <pnylander@stratiteq.com>
2019-10-07 13:48:45 +0200

    Added test project

---
+++ tests/Stratiteq.Extensions.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Stratiteq Sweden AB. All rights reserved.
+//
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+
+namespace Stratiteq.Extensions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
